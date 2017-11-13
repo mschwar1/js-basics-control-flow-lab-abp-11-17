542a2a98ad39ef18e50d28690b5e284e0ad63ece
Mai Schwartz <mai.schwartz@gmail.com>
2017-11-13 20:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,3 @@ function scuberGreetingForFeet(feet) {
 function ternaryCheckCity(city) {
    return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
 }
-switch switchOnCharmFromTip() {
-  case 'generous':
-    console.log('Thank you so much.')
-    break;
-  case 'not as generous':
-    console.log('Thank you.')
-    break;
-  default:
-    console.log('Bye.')
-}
